43f1fe538ae42d77c8087e814c7aa01224c99083
Mykael Mello <eng.mykael@gmail.com>
2024-10-31 12:22:41 -0300

    test custom http excpetion filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './exception_filters/http-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getHello(): string {
-    return this.appService.getHello();
+    throw new BadRequestException('Something bad happened', {
+      cause: new Error(),
+      description: 'Some error description',
+    });
+    // return this.appService.getHello();
   }
   @Post()
   setHello(): string {
